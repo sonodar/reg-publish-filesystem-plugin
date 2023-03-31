bc10a47cac2b7ba4bf8db989287dee84f7e589e5
園田 亮平 <ryohei-sonoda@m3.com>
2023-03-31 15:18:43 +0900

    fix eslint env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   root: true,
+  env: {
+    node: true,
+  },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
